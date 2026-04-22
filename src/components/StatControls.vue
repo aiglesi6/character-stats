abdeebb046b93cf1536b8cd29cc1397a36e3dbb2
aiglesi6 <a_iglesias_12@hotmail.com>
2026-04-22 18:19:44 -0500

    added stats

---
+++ src/components/StatControls.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>Points: {{ store.points }}</h2>
+
+    <div v-for="(value, key) in store.stats" :key="key">
+      {{ key }}: {{ value }}
+      <button @click="store.increaseStat(key)">+</button>
+      <button @click="store.decreaseStat(key)">-</button>
+    </div>
+
+    <button @click="store.resetCharacter">Reset</button>
+  </div>
+</template>
+
+<script>
+import { useCharacterStore } from '@/stores/character'
+
+export default {
+  setup() {
+    const store = useCharacterStore()
+    return { store }
+  }
+}
+</script>
