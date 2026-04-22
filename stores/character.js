f1645367a06d69e17064e8199ae475617885ee12
aiglesi6 <a_iglesias_12@hotmail.com>
2026-04-22 17:56:25 -0500

    added getters

--- stores/character.js
+++ stores/character.js
@@ -11,6 +11,20 @@ export const useCharacterStore = defineStore('character', {
     points: 10
   }),
 
+  getters: {
+    totalStats: (state) => {
+      return Object.values(state.stats).reduce((a, b) => a + b, 0)
+    },
+
+    powerLevel: (state) => {
+      return (
+        state.stats.strength * 2 +
+        state.stats.agility * 1.5 +
+        state.stats.intelligence * 1.8
+      )
+    }
+  },
+
   actions: {
     increaseStat(stat) {
       if (this.points > 0) {
