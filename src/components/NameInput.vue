31e0865be41f192a11b008b252341bc35fb2c3d7
aiglesi6 <a_iglesias_12@hotmail.com>
2026-04-22 18:10:05 -0500

    add character

---
+++ src/components/NameInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>Character Name</h2>
+    <input v-model="name" placeholder="Enter Name:" />
+  </div>
+</template>
+
+<script setup>
+import { useCharacterStore } from '../stores/character'
+import { computed } from 'vue'
+
+const store = useCharacterStore()
+
+const name = computed({
+  get: () => store.name,
+  set: (value) => store.setName(value)
+})
+</script>
