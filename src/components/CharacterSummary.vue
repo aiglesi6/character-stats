894fd4699f108438a4912e030d62b4b6ef079273
aiglesi6 <a_iglesias_12@hotmail.com>
2026-04-22 18:23:38 -0500

    added character summarys

---
+++ src/components/CharacterSummary.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>Summary</h2>
+
+    <p>Name: {{ store.name }}</p>
+
+    <ul>
+      <li v-for="(value, key) in store.stats" :key="key">
+        {{ key }}: {{ value }}
+      </li>
+    </ul>
+
+    <p>Total: {{ store.totalStats }}</p>
+    <p>Power: {{ store.powerLevel.toFixed(1) }}</p>
+  </div>
+</template>
+
+<script>
+import { useCharacterStore } from '@/stores/character'
+
+export default {
+  setup() {
+    const store = useCharacterStore()
+    return { store }
+  }
+}
+</script>
